a9e5797dbfb0e3e42dfa6d9251a00dbbdee67c9f
Ghosh <2263111@cognizant.com>
2023-05-29 18:04:36 +0530

    🔰first commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,56 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchMeal = async () => {
+      const response = await fetch("http://localhost:8090/menu");
+      const responseData = await response.json();
+
+      const loadedData = [];
+
+      for (const key in responseData) {
+        loadedData.push({
+          id: responseData[key].id,
+          name: responseData[key].item,
+          description: responseData[key].category,
+          price: responseData[key].price,
+        });
+        console.log(key);
+      }
+      console.log(
+        "-------------------------------------------------\n" +
+          loadedData +
+          "\n-------------------------------------------------"
+      );
+
+      setMeals(loadedData);
+    };
+
+    fetchMeal();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
